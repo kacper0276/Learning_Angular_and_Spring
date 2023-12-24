1d19823df1640745678ae423f3300db9849ec887
kacper0276 <kacper0276@op.pl>
2023-12-24 01:34:08 +0100

    @input, tworzenie komponentu dziecko - Następny odcinek: @Output, wyczyszczenie wiadomości o błędzie

---
+++ pierwszy-projekt/src/app/shared/components/alert/alert.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  standalone: true,
+  imports: [],
+  templateUrl: './alert.component.html',
+  styleUrl: './alert.component.css'
+})
+export class AlertComponent {
+  @Input() errorMessage!: string; // Input z rodzica do dziecka, Output z dziecka do rodzica
+}
--- pierwszy-projekt/src/app/todo-list/todo-list.component.ts
+++ pierwszy-projekt/src/app/todo-list/todo-list.component.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Todo } from '../shared/interfaces/todo.interface';
+import { AlertComponent } from '../shared/components/alert/alert.component';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AlertComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
   todos: Todo[] = [];
   errorMessage = "";
-  testSwitchCase = 'tak';
 
   addTodo(todo: string): void {
     if(todo.length <= 3) {
