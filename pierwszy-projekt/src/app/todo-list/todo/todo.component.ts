715f345c6683e186cb45c44774cade2d05855108
kacper0276 <kacper0276@op.pl>
2023-12-25 06:52:59 +0100

    Okno modalne (modal/pop up) - Następny odcinek: Projekcja treści (ng-content) w modalu

--- pierwszy-projekt/src/app/todo-list/todo/todo.component.ts
+++ pierwszy-projekt/src/app/todo-list/todo/todo.component.ts
@@ -1,19 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { Todo } from '../../shared/interfaces/todo.interface';
 import { CommonModule } from '@angular/common';
+import { ModalComponent } from '../../shared/components/modal/modal.component';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ModalComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
   @Input() todo!: Todo;
   @Input() i!: number;
+  openModal = false;
 
   changeTodoStatus(todo: Todo): void {
     todo.isComplete = !todo.isComplete;    
+  }
+
+  toogleModal(): void {
+    this.openModal = !this.openModal;
   }
 }
