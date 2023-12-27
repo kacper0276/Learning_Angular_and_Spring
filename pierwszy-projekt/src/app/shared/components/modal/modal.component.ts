d8b9338801ab781857aa86c28cedc5eacbd5b068
kacper0276 <kacper0276@op.pl>
2023-12-27 15:45:31 +0100

    ngAfterContentChecked (interfejs AfterContentChecked) - Następny odcinek: @ViewChild + ngAfterViewInit (interfejs AfterViewInit)

--- pierwszy-projekt/src/app/shared/components/modal/modal.component.ts
+++ pierwszy-projekt/src/app/shared/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, ContentChild, ElementRef, EventEmitter, Input, Output } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, ElementRef, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,17 +7,23 @@ import { AfterContentInit, Component, ContentChild, ElementRef, EventEmitter, In
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
-export class ModalComponent implements AfterContentInit{
+export class ModalComponent implements AfterContentInit, AfterContentChecked{
   @Input() title!: string;
   @Output() close = new EventEmitter<void>();
   @ContentChild('modalDiv') modalDiv!: ElementRef; 
+  @ContentChild('check') checkBox!: ElementRef;
 
   onClose() {
     this.close.emit();
   }
 
   ngAfterContentInit(): void {
-    console.log('ngAfterContentInit');
-    console.log(this.modalDiv);
+    // console.log('ngAfterContentInit');
+    // console.log(this.modalDiv);
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+    console.log(this.checkBox.nativeElement.checked);
   }
 }
