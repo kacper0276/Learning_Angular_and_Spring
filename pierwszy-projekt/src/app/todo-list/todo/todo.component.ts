3d4428b4c3da58a8a4b7a8afe3b302541fa20570
kacper0276 <kacper0276@op.pl>
2023-12-29 02:12:46 +0100

    Ustawienia lokalne i omówienie pipe, które z nich korzystają - Następny odcinek: Tworzenie nowego pipe + wytłumaczenie pure pipe / impure pipe

--- pierwszy-projekt/src/app/todo-list/todo/todo.component.ts
+++ pierwszy-projekt/src/app/todo-list/todo/todo.component.ts
@@ -1,14 +1,20 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, LOCALE_ID, Output, ViewChild } from '@angular/core';
 import { Todo } from '../../shared/interfaces/todo.interface';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { ModalComponent } from '../../shared/components/modal/modal.component';
+import localePl from '@angular/common/locales/pl'
+
+registerLocaleData(localePl);
 
 @Component({
   selector: 'app-todo',
   standalone: true,
   imports: [CommonModule, ModalComponent],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.css'
+  styleUrl: './todo.component.css',
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pl'}
+  ]
 })
 export class TodoComponent 
 // implements OnChanges
