3efd349404185e89c0f9c239facbbce8a7772451
kacper0276 <kacper0276@op.pl>
2024-01-03 10:25:04 +0100

    Wykorzystanie dyrektwywy ngModel w aplikacji - Następny odcinek: Omówienie Template Driven Forms

--- pierwszy-projekt/src/app/todo-list/add-todo-form/add-todo-form.component.ts
+++ pierwszy-projekt/src/app/todo-list/add-todo-form/add-todo-form.component.ts
@@ -1,16 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-todo-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-todo-form.component.html',
-  styleUrl: './add-todo-form.component.css'
+  styleUrl: './add-todo-form.component.css',
 })
 export class AddTodoFormComponent {
   @Output() addTodo = new EventEmitter<string>();
+  todoName = '';
 
-  addNewTodo(task: string) {
-    this.addTodo.emit(task);
+  addNewTodo() {
+    this.addTodo.emit(this.todoName);
   }
 }
