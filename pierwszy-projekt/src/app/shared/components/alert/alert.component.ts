3c72cbecfbc138fb55574ec09754b1ef7d5b099b
kacper0276 <kacper0276@op.pl>
2024-01-03 11:31:42 +0100

    Implementacja komunikatów o błędach walidacji - Następny odcinek: Komunikaty o błędach - 2 sposób

--- pierwszy-projekt/src/app/shared/components/alert/alert.component.ts
+++ pierwszy-projekt/src/app/shared/components/alert/alert.component.ts
@@ -5,11 +5,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './alert.component.html',
-  styleUrl: './alert.component.css'
+  styleUrl: './alert.component.css',
 })
 export class AlertComponent {
   @Input() errorMessage!: string; // Input z rodzica do dziecka, Output z dziecka do rodzica
   @Output() clearMessage = new EventEmitter<void>();
+  @Input() showButton = false;
 
   clearErrorMessage() {
     this.clearMessage.emit();
--- pierwszy-projekt/src/app/todo-list/add-todo-form/add-todo-form.component.ts
+++ pierwszy-projekt/src/app/todo-list/add-todo-form/add-todo-form.component.ts
@@ -1,18 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { AlertComponent } from '../../shared/components/alert/alert.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-todo-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, AlertComponent, CommonModule],
   templateUrl: './add-todo-form.component.html',
   styleUrl: './add-todo-form.component.css',
 })
-export class AddTodoFormComponent {
+export class AddTodoFormComponent implements AfterViewInit {
   @Output() addTodo = new EventEmitter<string>();
+  @ViewChild('form') todoForm!: NgForm;
   todoName = '';
 
-  addNewTodo() {
+  ngAfterViewInit(): void {
+    console.log(this.todoForm);
+  }
+
+  addNewTodo(form: NgForm) {
+    console.log(form);
     this.addTodo.emit(this.todoName);
   }
 }
