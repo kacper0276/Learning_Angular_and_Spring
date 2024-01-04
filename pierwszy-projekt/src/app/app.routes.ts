7164be8ac28ec70c50950084ff6e334ea2615faa
kacper0276 <kacper0276@op.pl>
2024-01-04 14:18:42 +0100

    Dodanie parametrów do ścieżki (route'a) - Następny odcinek: TodoService - metoda zwracająca szukane zadanie

--- pierwszy-projekt/src/app/app.routes.ts
+++ pierwszy-projekt/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
 
 export const routes: Routes = [
   //   { path: '', redirectTo: '/home', pathMatch: 'full' },
   //   { path: 'home', component: HomeComponent },
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'todo', component: TodoListComponent, title: 'Dodaj zadanie!' },
+  {
+    path: 'todo/:id',
+    component: TodoDetailsComponent,
+    title: 'Szczegóły zadania',
+  },
   { path: '**', component: PageNotFoundComponent, title: 'Page not found' },
 ];
