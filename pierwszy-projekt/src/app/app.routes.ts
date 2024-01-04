3d034b0be5d5c33ef0ce1fed6c31eb0824cf4ba1
kacper0276 <kacper0276@op.pl>
2024-01-04 12:04:07 +0100

    Dlaczego pathMatch z opcją full a nie prefix? + redirectTo - Następny odcinek: Page not found (wykorzystanie wildcard)

--- pierwszy-projekt/src/app/app.routes.ts
+++ pierwszy-projekt/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'todo', component: TodoListComponent },
 ];
