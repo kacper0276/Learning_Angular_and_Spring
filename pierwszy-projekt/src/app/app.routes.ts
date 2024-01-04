cbd5fe043aa7508ceb780583cba51e1b4517ebb3
kacper0276 <kacper0276@op.pl>
2024-01-04 10:07:28 +0100

    Definiowanie ścieżek (route'ów) - Następny odcinek: Dlaczego pathMatch z opcją full a nie prefix? + redirectTo

--- pierwszy-projekt/src/app/app.routes.ts
+++ pierwszy-projekt/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-export const routes: Routes = [{}];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'todo', component: TodoListComponent },
+];
