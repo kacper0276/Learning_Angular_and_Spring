3986a9599441b53c56baa999199887b2cc99ba9b
kacper0276 <kacper0276@op.pl>
2024-01-04 12:26:03 +0100

    Page not found (wykorzystanie wildcard) - Następny odcinek: Tytuł zakładki - ustawienie title dla pojedyńczego route'a (Angular 14)

--- pierwszy-projekt/src/app/app.routes.ts
+++ pierwszy-projekt/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  //   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  //   { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'todo', component: TodoListComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
