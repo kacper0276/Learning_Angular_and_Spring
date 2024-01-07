2b4aa6342c4737d07ba6a367995150a55bebf0c0
kacper0276 <kacper0276@op.pl>
2024-01-07 01:57:16 +0100

    Utworzenie header (Angular Material) - Następny odcinek: MaterialModule

--- rejestr-klientow/src/app/modules/core/core.module.ts
+++ rejestr-klientow/src/app/modules/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
