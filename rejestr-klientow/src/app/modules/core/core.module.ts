b4840c1ff33de9523e78905ff3dcf88bf07e1a5d
kacper0276 <kacper0276@op.pl>
2024-01-08 08:11:07 +0100

    MaterialModule - Następny odcinek: HomeModule (FeatureModule) + routing w module home

--- rejestr-klientow/src/app/modules/core/core.module.ts
+++ rejestr-klientow/src/app/modules/core/core.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../shared/material/material.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MaterialModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
