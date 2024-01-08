9a8601b5b0b2f272dfa6822ff487a852764d4414
kacper0276 <kacper0276@op.pl>
2024-01-08 08:41:21 +0100

    SharedModule - Następny odcinek: Ważne! Moduł Uwierzytelniania

--- rejestr-klientow/src/app/modules/clients/clients.module.ts
+++ rejestr-klientow/src/app/modules/clients/clients.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ClientsRoutingModule } from './clients-routing.module';
 import { ClientsComponent } from './clients.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ClientsComponent],
-  imports: [CommonModule, ClientsRoutingModule],
+  imports: [SharedModule, ClientsRoutingModule],
   exports: [ClientsComponent],
 })
 export class ClientsModule {}
