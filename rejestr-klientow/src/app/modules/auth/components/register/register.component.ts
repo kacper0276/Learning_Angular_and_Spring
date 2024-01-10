27977c61b47e5b29d4e9f75295aeaf47f16cca62
kacper0276 <kacper0276@op.pl>
2024-01-10 14:13:17 +0100

    Śledzenie zmian w kontrolce (valueChanges) - Następny odcinek: Dynamiczne wyłączanie i włączanie kontrolki (enable(), disable())

--- rejestr-klientow/src/app/modules/auth/components/register/register.component.ts
+++ rejestr-klientow/src/app/modules/auth/components/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,13 +6,19 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   hide = true;
   registerForm = new FormGroup({
     email: new FormControl(''),
     username: new FormControl(''),
     password: new FormControl(''),
   });
+
+  ngOnInit(): void {
+    this.registerForm.controls.email.valueChanges.subscribe((text) => {
+      console.log(text);
+    });
+  }
 
   onRegister() {
     //
