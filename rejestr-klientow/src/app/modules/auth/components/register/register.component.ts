da416d8203460eb61e10ee51df2935f4c474dd6b
kacper0276 <kacper0276@op.pl>
2024-01-11 14:58:24 +0100

    Modyfikacja wartości FormControl / FormGroup (w kodzie TS) - Następny odcinek: AuthService - rejestracja (POST) + opcja nonNullable w FormControl

--- rejestr-klientow/src/app/modules/auth/components/register/register.component.ts
+++ rejestr-klientow/src/app/modules/auth/components/register/register.component.ts
@@ -8,20 +8,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   hide = true;
-  registerForm = new FormGroup(
-    {
-      email: new FormControl('', [
-        Validators.email,
-        Validators.minLength(5),
-        Validators.maxLength(50),
-      ]),
-      username: new FormControl('', [Validators.required]),
-      password: new FormControl('', { validators: [Validators.required] }),
-    },
-    {
-      updateOn: 'submit',
-    },
-  );
+  registerForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.email,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', { validators: [Validators.required] }),
+  });
 
   get controls() {
     return this.registerForm.controls;
@@ -31,12 +26,23 @@ export class RegisterComponent implements OnInit {
     // this.registerForm.controls.email.valueChanges.subscribe((text) => {
     //   console.log(text);
     // });
-    console.log('');
     this.controls.username.addValidators(Validators.minLength(5)); // Dodaje do obecnych validatorów
     // Starszy sposób
     // this.controls.username.setValidators([Validators.minLength(5), Validators.required]); // Nadpisuje stare validatory
 
     // this.registerForm.controls.email.disable();
+
+    // this.controls.username.setValue('test1');
+    // this.registerForm.setValue({
+    //   email: 'test@ad',
+    //   username: '',
+    //   password: '',
+    // });
+
+    // patchValue - nie trzeba przekazywać całego obiektu
+    // this.registerForm.patchValue({
+    //   email: 'Sigma@op',
+    // });
   }
 
   // enableControl() {
