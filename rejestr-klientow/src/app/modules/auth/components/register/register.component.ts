5dd8c882b1baf21561c3b5afe5b39327272e91c6
kacper0276 <kacper0276@op.pl>
2024-01-11 12:19:50 +0100

    Walidacja w Reactive Form (z Angular Material) - Następny odcinek: Dodawanie/zmienianie walidatorów (setValidators(), addValidators())

--- rejestr-klientow/src/app/modules/auth/components/register/register.component.ts
+++ rejestr-klientow/src/app/modules/auth/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,22 +9,47 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   hide = true;
   registerForm = new FormGroup({
-    email: new FormControl(''),
-    username: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [
+      Validators.email,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', { validators: [Validators.required] }),
   });
+
+  get controls() {
+    return this.registerForm.controls;
+  }
 
   ngOnInit(): void {
     // this.registerForm.controls.email.valueChanges.subscribe((text) => {
     //   console.log(text);
     // });
     console.log('');
+
     // this.registerForm.controls.email.disable();
   }
 
   // enableControl() {
   //   this.registerForm.controls.email.enable();
   // }
+
+  getErrorMessage(control: FormControl) {
+    if (control.hasError('required')) {
+      return 'Musisz podać jakąś wartość.';
+    }
+
+    if (control.hasError('minlength')) {
+      return 'Przekazałeś za mało znaków w kontrolce';
+    }
+
+    if (control.hasError('maxlength')) {
+      return 'Przekazałeś za dużo znaków w kontrolce';
+    }
+
+    return control.hasError('email') ? 'Nieprawidłowy adres email' : '';
+  }
 
   onRegister() {
     // console.log(this.registerForm.value); // email - wylączona zawiera:  password: "", username: ""
