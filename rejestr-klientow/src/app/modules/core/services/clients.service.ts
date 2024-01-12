977b18981a71ad740bd56e74e804063b47d5122c
kacper0276 <kacper0276@op.pl>
2024-01-12 11:03:30 +0100

    Utworzenie i praca z ClientsService - Następny odcinek: Przedstawienie mat-table (Angular Material, paginacja serwerowa, sortowanie)

---
+++ rejestr-klientow/src/app/modules/core/services/clients.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { Observable, map } from 'rxjs';
+import { Client, ClientResponse, PostClient } from '../models/client.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClientsService {
+  apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getClients(): Observable<Client[]> {
+    return this.http
+      .get<ClientResponse[]>(`${this.apiUrl}/clients`)
+      .pipe(
+        map((clients) =>
+          clients.map(
+            ({ id, firstname, surname, email, phone, address, postcode }) =>
+              new Client(
+                id,
+                firstname,
+                surname,
+                email,
+                phone,
+                address,
+                postcode,
+              ),
+          ),
+        ),
+      );
+  }
+
+  postClient(clientData: PostClient): Observable<Client> {
+    return this.http
+      .post<ClientResponse>(`${this.apiUrl}/clients`, clientData)
+      .pipe(
+        map(
+          ({ id, firstname, surname, email, phone, address, postcode }) =>
+            new Client(id, firstname, surname, email, phone, address, postcode),
+        ),
+      );
+  }
+}
