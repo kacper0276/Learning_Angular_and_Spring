1be230ff04a2dbd0122a085fa1f4b7ce51a8a206
kacper0276 <kacper0276@op.pl>
2024-01-12 01:12:06 +0100

    Dynamiczne dodawanie kontrolek - FormArray - Następny odcinek: Utworzenie client.model.ts

--- rejestr-klientow/src/app/modules/auth/components/register/register.component.ts
+++ rejestr-klientow/src/app/modules/auth/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../../core/services/auth.service';
 import { PostUser } from '../../../core/models/user.model';
 import { Router } from '@angular/router';
@@ -29,6 +29,7 @@ export class RegisterComponent implements OnInit {
       validators: [Validators.required],
       nonNullable: true,
     }),
+    hobbies: new FormArray([new FormControl('')]),
   });
 
   constructor(
@@ -38,6 +39,18 @@ export class RegisterComponent implements OnInit {
 
   get controls() {
     return this.registerForm.controls;
+  }
+
+  get hobbies() {
+    return this.registerForm.get('hobbies') as FormArray;
+  }
+
+  addControl() {
+    this.hobbies.push(new FormControl(''));
+  }
+
+  removeControl(index: number) {
+    this.hobbies.removeAt(index);
   }
 
   ngOnInit(): void {
