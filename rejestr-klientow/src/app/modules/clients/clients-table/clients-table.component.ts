31809fa37b381c65baf980cfd7e01622b0581a92
kacper0276 <kacper0276@op.pl>
2024-01-13 05:17:06 +0100

    Wykorzystanie mat-table do wyświetlania klientów - Następny odcinek: Wykorzystanie paginacji po stronie serwera (modyfikacja metody getClients())

--- rejestr-klientow/src/app/modules/clients/clients-table/clients-table.component.ts
+++ rejestr-klientow/src/app/modules/clients/clients-table/clients-table.component.ts
@@ -1,5 +1,9 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { ClientsService } from '../../core/services/clients.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Client } from '../../core/models/client.model';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-clients-table',
@@ -7,16 +11,34 @@ import { ClientsService } from '../../core/services/clients.service';
   styleUrl: './clients-table.component.scss',
 })
 export class ClientsTableComponent implements AfterViewInit {
+  displayedColumns: string[] = ['lp', 'firstname', 'surname', 'email', 'buttons'];
+  dataSource!: MatTableDataSource<Client>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(private clientsService: ClientsService) {}
 
   ngAfterViewInit(): void {
     this.clientsService.getClients().subscribe({
       next: (clients) => {
-        console.log(clients);
+        this.dataSource = new MatTableDataSource<Client>(clients);
+
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       },
       error: (err) => {
         console.log(err);
       },
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
