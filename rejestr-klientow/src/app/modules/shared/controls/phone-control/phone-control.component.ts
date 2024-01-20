0640b8d768502bef6720c32d77ce4556199023ac
kacper0276 <kacper0276@op.pl>
2024-01-20 12:51:13 +0100

    Własna kontrolka dla numeru telefonu cz.2 - Następny odcinek: Czym jest Lazy loading?

--- rejestr-klientow/src/app/modules/shared/controls/phone-control/phone-control.component.ts
+++ rejestr-klientow/src/app/modules/shared/controls/phone-control/phone-control.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import {
   ControlValueAccessor,
   FormControl,
   NG_VALUE_ACCESSOR,
+  Validators,
 } from '@angular/forms';
-import { combineLatest } from 'rxjs';
+import { Subscription, combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-phone-control',
@@ -18,24 +19,28 @@ import { combineLatest } from 'rxjs';
     },
   ],
 })
-export class PhoneControlComponent implements ControlValueAccessor {
-  numberPrefixControl = new FormControl('');
-  numberControl = new FormControl('');
+export class PhoneControlComponent implements ControlValueAccessor, OnDestroy {
+  numberPrefixControl = new FormControl('', [Validators.required]);
+  numberControl = new FormControl('', [Validators.required]);
+  sub = new Subscription();
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   onChange = (value: string | null) => {};
   onTouch = () => {};
 
   constructor() {
-    combineLatest([
-      this.numberPrefixControl.valueChanges,
-      this.numberControl.valueChanges,
-    ]).subscribe(([prefix, number]) => {
-      if (prefix && number) {
-        this.onChange(`+${prefix}${number}`);
-      } else {
-        this.onChange(null);
-      }
-    });
+    this.sub.add(
+      combineLatest([
+        this.numberPrefixControl.valueChanges,
+        this.numberControl.valueChanges,
+      ]).subscribe(([prefix, number]) => {
+        if (prefix && number) {
+          this.onChange(`+${prefix}${number}`);
+        } else {
+          this.onChange(null);
+        }
+      }),
+    );
   }
 
   registerOnChange(fn: () => void): void {
@@ -45,8 +50,27 @@ export class PhoneControlComponent implements ControlValueAccessor {
   registerOnTouched(fn: () => void): void {
     this.onTouch = fn;
   }
-  writeValue(obj: any): void {}
+  writeValue(value: string): void {
+    const valueWithoutPlus = value.replace('+', '');
+    const prefix = valueWithoutPlus.slice(0, 2);
+    const number = valueWithoutPlus.slice(2);
+
+    this.numberPrefixControl.setValue(prefix);
+    this.numberControl.setValue(number);
+  }
 
   // Ta nie jest wymagana
-  setDisabledState?(isDisabled: boolean): void {}
+  setDisabledState?(isDisabled: boolean): void {
+    if (isDisabled) {
+      this.numberControl.disable();
+      this.numberPrefixControl.disable();
+    } else {
+      this.numberControl.enable();
+      this.numberPrefixControl.enable();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
