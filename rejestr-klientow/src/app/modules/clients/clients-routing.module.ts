aeabe4cb5e76491c9a4787de1b5a0771813737a0
kacper0276 <kacper0276@op.pl>
2024-01-21 01:53:19 +0100

    canActivate w postaci funkcji (Angular 15) - Następny odcinek: Guard canLoad i canMatch (w postaci funkcji)

--- rejestr-klientow/src/app/modules/clients/clients-routing.module.ts
+++ rejestr-klientow/src/app/modules/clients/clients-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientsComponent } from './clients.component';
 import { ClientComponent } from './components/client/client.component';
 import { ClientFormComponent } from './components/client-form/client-form.component';
-import { AuthActivateGuard } from '../core/guards/auth-activate.guard';
+import { authGuardActivate } from '../core/guards/auth-activate.function.guard';
 
 const routes: Routes = [
-  { path: '', component: ClientsComponent, canActivate: [AuthActivateGuard] },
+  { path: '', component: ClientsComponent, canActivate: [authGuardActivate] },
   { path: 'dodaj', component: ClientFormComponent },
   { path: ':id', component: ClientComponent },
 ];
