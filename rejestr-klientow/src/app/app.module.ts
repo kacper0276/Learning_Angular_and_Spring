22928a90374b7b85bddb6013d0e60784bc017538
kacper0276 <kacper0276@op.pl>
2024-01-24 19:25:07 +0100

    Utworzenie standalone component - footer - Następny odcinek: Standalone directive

--- rejestr-klientow/src/app/app.module.ts
+++ rejestr-klientow/src/app/app.module.ts
@@ -6,15 +6,23 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { FooterComponent } from './modules/core/components/footer/footer.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, AuthModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: !isDevMode(),
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    AuthModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    FooterComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
