b113f2a60ec0884f4b6d38306f7451eb0011e3c8
kacper0276 <kacper0276@op.pl>
2024-01-24 14:47:20 +0100

    Własna dyrektywa atrybutowa i @HostListener - Następny odcinek: Tworzenie własnej dyrektywy strukturalnej

---
+++ rejestr-klientow/src/app/modules/shared/directives/highlight.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+})
+export class HighlightDirective {
+  @Input() appHighlight = '';
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mouseenter') enter() {
+    this.el.nativeElement.style.color = this.appHighlight;
+  }
+
+  @HostListener('mouseleave') leave() {
+    this.el.nativeElement.style.color = '';
+  }
+
+  // ngOnInit(): void {
+  //   this.el.nativeElement.style.color = this.appHighlight;
+  // }
+}
--- rejestr-klientow/src/app/modules/shared/shared.module.ts
+++ rejestr-klientow/src/app/modules/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { MaterialModule } from './material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AlertComponent } from './components/alert/alert.component';
 import { PhoneControlComponent } from './controls/phone-control/phone-control.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
-  declarations: [AlertComponent, PhoneControlComponent],
+  declarations: [AlertComponent, PhoneControlComponent, HighlightDirective],
   imports: [CommonModule, ReactiveFormsModule, MaterialModule],
   exports: [
     CommonModule,
@@ -15,6 +16,7 @@ import { PhoneControlComponent } from './controls/phone-control/phone-control.co
     ReactiveFormsModule,
     AlertComponent,
     PhoneControlComponent,
+    HighlightDirective,
   ],
 })
 export class SharedModule {}
