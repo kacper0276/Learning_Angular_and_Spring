f86c692b568f050bf604312b78ec3524a3e6f844
kacper0276 <kacper0276@op.pl>
2024-01-24 15:39:37 +0100

    Dodanie Service worker i naprawa legacyPeerDeps - Następny odcinek: Konfiguracja Service worker - cache danych z backend'u i asset'ów z url

--- rejestr-klientow/src/app/app.module.ts
+++ rejestr-klientow/src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, AuthModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, AuthModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
