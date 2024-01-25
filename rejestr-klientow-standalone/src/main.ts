003563545c066c328f2c4da97843dd42d37008cd
kacper0276 <kacper0276@op.pl>
2024-01-25 15:46:08 +0100

    importProvidersFrom i migracja CoreModule - Następny odcinek: Routing w aplikacjach standalone (migracja HomeRoutingModule)

--- rejestr-klientow-standalone/src/main.ts
+++ rejestr-klientow-standalone/src/main.ts
@@ -1,21 +1,40 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
 import { AppComponent } from './app/app.component';
 import { isDevMode, importProvidersFrom } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AuthModule } from './app/modules/auth/auth.module';
-import { CoreModule } from './app/modules/core/core.module';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { SpinnerInterseptor } from './app/modules/core/interceptors/spinner.interceptor';
+import { HeaderClassInterseptor } from './app/modules/core/interceptors/header.class.interceptor';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, CoreModule, AuthModule, ServiceWorkerModule.register('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            // Register the ServiceWorker as soon as the application is stable
-            // or after 30 seconds (whichever comes first).
-            registrationStrategy: 'registerWhenStable:30000',
-        }))]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      HttpClientModule,
+      AuthModule,
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000',
+      }),
+    ),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HeaderClassInterseptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SpinnerInterseptor,
+      multi: true,
+    },
+  ],
+}).catch((err) => console.error(err));
