9dccb23028ec9b483331310edb5a12a7581bf648
kacper0276 <kacper0276@op.pl>
2024-01-25 18:02:52 +0100

    Dodanie NgRx store do projektu - Następny odcinek: Utworzenie reducer'a, dodanie initialState i pobieranie wartości ze store

--- pierwszy-projekt/src/app/app.config.ts
+++ pierwszy-projekt/src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())],
+  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideStore()],
 };
