cc4ad61e3eea4f94d3ad9f0043d4919637275fa7
kacper0276 <kacper0276@op.pl>
2024-01-26 10:20:44 +0100

    Dodanie @ngrx/effects do projektu - Następny odcinek: Utworzenie akcji do pobierania zadań z backendu z wykorzystaniem effect

--- pierwszy-projekt/src/app/app.config.ts
+++ pierwszy-projekt/src/app/app.config.ts
@@ -5,12 +5,17 @@ import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { StoreModule, provideStore } from '@ngrx/store';
 import { todoListReducer } from './todo-list/store/todo-list.reducer';
+import { EffectsModule, provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideStore(),
-    importProvidersFrom(StoreModule.forRoot({ todos: todoListReducer })),
+    importProvidersFrom(
+      StoreModule.forRoot({ todos: todoListReducer }),
+      EffectsModule.forRoot([])
+    ),
+    provideEffects(),
   ],
 };
