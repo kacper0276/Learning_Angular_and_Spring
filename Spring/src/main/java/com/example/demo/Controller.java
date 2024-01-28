b11153024a084633e28c9a029da7ff670ccad0d6
kacper0276 <kacper0276@op.pl>
2024-01-28 14:47:39 +0100

    @RestController - Następny odcinek: Implementacja metody GET cz. 1

---
+++ Spring/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1") // Endpointy przed adresem będą miały "/api/v1"
+public class Controller {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Testowe API";
+    }
+}
