2116061b5c90af0ff1d8b11220ce49edaf1c4607
kacper0276 <kacper0276@op.pl>
2024-01-28 15:10:32 +0100

    Implementacja metody GET cz. 2 - Następny odcinek: Implementacja metody POST

--- Spring/src/main/java/com/example/demo/FamilyController.java
+++ Spring/src/main/java/com/example/demo/FamilyController.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.UUID;
@@ -26,6 +23,20 @@ public class FamilyController {
         families.add(new Family(UUID.randomUUID().toString(), "Nowak", members));
 
         return families;
+    }
+
+    @GetMapping("/getByName")
+    public Family getByName(@RequestParam String familyName) {
+        ArrayList<Family> families = new ArrayList<>();
+        ArrayList<Member> members = new ArrayList<>();
+        members.add(new Member("Adam", 12, "M"));
+        members.add(new Member("Wojtek", 16, "M"));
+        members.add(new Member("Kasia", 24, "K"));
+
+        families.add(new Family(UUID.randomUUID().toString(), "Kowalski", members));
+        families.add(new Family(UUID.randomUUID().toString(), "Nowak", members));
+
+        return families.stream().filter(family -> family.getName().equals(familyName)).findFirst().orElseThrow();
     }
 
 }
