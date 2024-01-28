c072094845bb1c16ee11d369613f22b4ec90cb1e
kacper0276 <kacper0276@op.pl>
2024-01-28 15:20:07 +0100

    Implementacja metody POST cz. 2 - Następny odcinek: Implementacja metody PATCH

--- Spring/src/main/java/com/example/demo/FamilyController.java
+++ Spring/src/main/java/com/example/demo/FamilyController.java
@@ -1,20 +1,22 @@
 package com.example.demo;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/api/v1/family")
 public class FamilyController {
+    ArrayList<Family> families = new ArrayList<>();
+    ArrayList<Member> members = new ArrayList<>();
 
 //  @GetMapping("/getall")
     // Lub
     @RequestMapping(value = "/getall", method = RequestMethod.GET)
     public ArrayList<Family> getAll() {
-        ArrayList<Family> families = new ArrayList<>();
-        ArrayList<Member> members = new ArrayList<>();
         members.add(new Member("Adam", 12, "M"));
         members.add(new Member("Wojtek", 16, "M"));
         members.add(new Member("Kasia", 24, "K"));
@@ -27,8 +29,6 @@ public class FamilyController {
 
     @GetMapping("/getByName")
     public Family getByName(@RequestParam String familyName) {
-        ArrayList<Family> families = new ArrayList<>();
-        ArrayList<Member> members = new ArrayList<>();
         members.add(new Member("Adam", 12, "M"));
         members.add(new Member("Wojtek", 16, "M"));
         members.add(new Member("Kasia", 24, "K"));
@@ -37,6 +37,16 @@ public class FamilyController {
         families.add(new Family(UUID.randomUUID().toString(), "Nowak", members));
 
         return families.stream().filter(family -> family.getName().equals(familyName)).findFirst().orElseThrow();
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public void createFamily(@RequestBody Family family, HttpServletResponse response) throws IOException {
+        if(family.getName() != null && !family.getMembers().isEmpty()) {
+           families.add(family);
+           response.sendError(HttpServletResponse.SC_OK, "Dodano do listy");
+           return;
+        }
+        response.sendError(HttpServletResponse.SC_CONFLICT, "Nazwa rodziny nie może być pusta");
     }
 
 }
