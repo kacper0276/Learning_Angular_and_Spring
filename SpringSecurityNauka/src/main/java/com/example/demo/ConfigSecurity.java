0be682f85ac92e48e347cdf1c7d4bd83bd6fd751
kacper0276 <kacper0276@op.pl>
2024-02-03 00:46:48 +0100

    Utworzenie własnej konfiguracji - Następny odcinek: Zarządzanie dostępem do endpointów

---
+++ SpringSecurityNauka/src/main/java/com/example/demo/ConfigSecurity.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+            authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
+        }).build();
+    }
+}
