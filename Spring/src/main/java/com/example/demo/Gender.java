a7753cbde4da9f9d09c2ac5e3a440a59da517ab2
kacper0276 <kacper0276@op.pl>
2024-02-02 11:45:01 +0100

    Użycie enum - Następny odcinek: Automatyczne generowanie wartości dla parametru id

---
+++ Spring/src/main/java/com/example/demo/Gender.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+public enum Gender {
+    WOMEN,
+    MEN
+}
--- Spring/src/main/java/com/example/demo/MembersDB.java
+++ Spring/src/main/java/com/example/demo/MembersDB.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,14 +12,16 @@ import lombok.Setter;
 @AllArgsConstructor
 public class MembersDB {
     @Id
+
     @Column(name = "id")
     private long id;
     @Column(name = "name")
     private String name;
     @Column(name = "age")
     private int age;
+    @Enumerated(EnumType.STRING)
     @Column(name = "gender")
-    private String gender;
+    private Gender gender;
     @Column(name = "family_id")
     private long familyId;
 }
