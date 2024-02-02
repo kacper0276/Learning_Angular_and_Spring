22dff5251220848789b845e3df893a6402f952de
kacper0276 <kacper0276@op.pl>
2024-02-03 00:17:01 +0100

    Mapowanie poszczególnych pól - Następny odcinek: Utworzenie własnej metody mapującej

--- Spring/src/main/java/com/example/demo/FamilyMapper.java
+++ Spring/src/main/java/com/example/demo/FamilyMapper.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper
 public abstract class FamilyMapper {
+    @Mappings({
+            @Mapping(target = "name", source = "name"),
+            @Mapping(target = "uid", source = "id"),
+    })
     public abstract Family FamilyDbToFamily(FamilyDB familyDB);
 }
