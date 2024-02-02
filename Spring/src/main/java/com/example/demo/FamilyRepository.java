94e1c2d51445c65aa01f31c6c6ff8449eba23791
kacper0276 <kacper0276@op.pl>
2024-02-02 12:30:15 +0100

    Utworzenie swojej metody w repozytorium - Następny odcinek: Utworzenie metody w repozytorium zapytaniem natywnym

--- Spring/src/main/java/com/example/demo/FamilyRepository.java
+++ Spring/src/main/java/com/example/demo/FamilyRepository.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FamilyRepository extends JpaRepository<FamilyDB, Long> {
+    Optional<FamilyDB> findById(Long id);
+
+    Iterable<FamilyDB> findByNameAndOrigin(String name, String origin);
 }
