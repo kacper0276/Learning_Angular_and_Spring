5101cc910b0302d184197d81ad2b4c34480ca132
kacper0276 <kacper0276@op.pl>
2024-02-02 20:32:26 +0100

    Dodawanie parametrów - Następny odcinek: Dodawanie body żądania

--- Spring/src/main/java/com/example/demo/RestTemplateApi.java
+++ Spring/src/main/java/com/example/demo/RestTemplateApi.java
@@ -1,25 +1,40 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class RestTemplateApi {
     RestTemplate restTemplate;
+
+    @Value("${ninja.api.quotes}")
+    String url;
 
     public RestTemplateApi(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public String getQuotes() throws URISyntaxException {
-        URI api_url = new URI("https://api.api-ninjas.com/v1/quotes?category=happiness");
+        Map<String, String> params = new HashMap<>();
+        params.put("category", "happiness");
+
+        URI api_url = UriComponentsBuilder.fromUriString(url)
+                .queryParam("category", params.get("category"))
+                .build()
+                .toUri();
+
+//        URI api_url = new URI("?category=happiness");
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-Api-Key", "VOCI7sIXVR5rNp8Z2WW5cw==nDwd2CCndNPW1FH6");
         HttpEntity<String> request = new HttpEntity<>(headers);
