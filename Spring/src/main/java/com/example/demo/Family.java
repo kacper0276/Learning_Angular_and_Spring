16c01d49ee4cf6c3acc4ac72ee71b61d27d06410
kacper0276 <kacper0276@op.pl>
2024-02-02 10:33:57 +0100

    Utworzenie konstruktorów - Następny odcinek: Zapoznanie z @Data oraz @toString

--- Spring/src/main/java/com/example/demo/Family.java
+++ Spring/src/main/java/com/example/demo/Family.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,14 +8,14 @@ import java.util.ArrayList;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Family {
     private final String uid;
     private String name;
     private ArrayList<Member> members;
 
-    public Family(String uid, String name, ArrayList<Member> members) {
+    public Family(String uid, String name) {
         this.uid = uid;
         this.name = name;
-        this.members = members;
     }
 }
