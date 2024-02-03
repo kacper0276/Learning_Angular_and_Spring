1bc08a38b740e590f9788669fac911a839e33193
kacper0276 <kacper0276@op.pl>
2024-02-03 01:43:35 +0100

    Zmiana w modelu - Następny odcinek: UserEntityUserDetailsService

--- SpringSecurityNauka/src/main/java/com/example/demo/UserEntityUserDetails.java
+++ SpringSecurityNauka/src/main/java/com/example/demo/UserEntityUserDetails.java
@@ -19,7 +19,7 @@ public class UserEntityUserDetails implements UserDetails {
     public UserEntityUserDetails(UserEntity userEntity) {
         this.mail = userEntity.getMail();
         this.password = userEntity.getPassword();
-        this.isEnabled = !userEntity.isLoginDisabled();
+        this.isEnabled = userEntity.isLoginDisabled();
         this.authority = Arrays.stream(userEntity.getRoles().split(","))
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
