f2c9ce6c5d1c4d96270252c382f3351f2822406d
kacper0276 <kacper0276@op.pl>
2024-02-03 01:35:00 +0100

    @EnableMethodSecurity - Następny odcinek: UserEntityUserDetails

--- SpringSecurityNauka/src/main/java/com/example/demo/ConfigSecurity.java
+++ SpringSecurityNauka/src/main/java/com/example/demo/ConfigSecurity.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class ConfigSecurity {
 
     @Bean
