5a876ec4558f67bfe16fd8d478de0dea86e9ef8d
kacper0276 <kacper0276@op.pl>
2024-02-03 22:30:14 +0100

    Implementacja Fasady - Następny odcinek: Utworzenie modeli bazodanowych

---
+++ DobrePraktykiProjektu/src/main/java/com/example/demo/fasada/FamilyController.java
@@ -0,0 +1,13 @@
+package com.example.demo.fasada;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FamilyController {
+
+    public void saveFamily() {}
+
+    public void getFamilyByParam() {}
+
+    public void getAllFamily() {}
+}
