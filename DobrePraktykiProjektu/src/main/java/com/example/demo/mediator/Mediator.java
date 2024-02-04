13a23180e6acd86851ca733fb96337a28c9ea86d
kacper0276 <kacper0276@op.pl>
2024-02-05 00:16:27 +0100

    Stworzenie serwisu Members - Następny odcinek: Utworzenie translatora dla modelu bazy danych

--- DobrePraktykiProjektu/src/main/java/com/example/demo/mediator/Mediator.java
+++ DobrePraktykiProjektu/src/main/java/com/example/demo/mediator/Mediator.java
@@ -3,7 +3,9 @@ package com.example.demo.mediator;
 import com.example.demo.model.FamilyDB;
 import com.example.demo.model.FamilyDTO;
 import com.example.demo.model.FamilyExtendedDTO;
+import com.example.demo.model.MembersDTO;
 import com.example.demo.services.FamilyService;
+import com.example.demo.services.MemberService;
 import com.example.demo.translator.TranslatorFamilyDBtoFamilyDTO;
 import com.example.demo.translator.TranslatorFamilyDTOtoFamilyDB;
 import com.example.demo.translator.TranslatorMemberDBToMemberDTO;
@@ -15,12 +17,14 @@ public class Mediator{
     TranslatorFamilyDBtoFamilyDTO translatorFamilyDBtoFamilyDTO;
     TranslatorFamilyDTOtoFamilyDB translatorFamilyDTOtoFamilyDB;
     FamilyService familyService;
+    MemberService memberService;
 
-    public Mediator(TranslatorMemberDBToMemberDTO translatorMemberDBToMemberDTO, TranslatorFamilyDBtoFamilyDTO translatorFamilyDBtoFamilyDTO, FamilyService familyService, TranslatorFamilyDTOtoFamilyDB translatorFamilyDTOtoFamilyDB) {
+    public Mediator(TranslatorMemberDBToMemberDTO translatorMemberDBToMemberDTO, TranslatorFamilyDBtoFamilyDTO translatorFamilyDBtoFamilyDTO, FamilyService familyService, TranslatorFamilyDTOtoFamilyDB translatorFamilyDTOtoFamilyDB, MemberService memberService) {
         this.translatorMemberDBToMemberDTO = translatorMemberDBToMemberDTO;
         this.translatorFamilyDBtoFamilyDTO = translatorFamilyDBtoFamilyDTO;
         this.translatorFamilyDTOtoFamilyDB = translatorFamilyDTOtoFamilyDB;
         this.familyService = familyService;
+        this.memberService = memberService;
     }
 
     public void saveFamily(FamilyDTO familyDTO) {
@@ -31,5 +35,14 @@ public class Mediator{
     public void updateFamily(FamilyExtendedDTO familyExtendedDTO) {
         FamilyDB familyDB = translatorFamilyDTOtoFamilyDB.toFamilyDB(familyExtendedDTO);
         familyService.save(familyDB);
+    }
+
+    public void saveMember(MembersDTO membersDTO) {
+
+        memberService.save(null);
+    }
+
+    public void updateMember(MembersDTO membersDTO) {
+        memberService.save(null);
     }
 }
---
+++ DobrePraktykiProjektu/src/main/java/com/example/demo/services/MemberService.java
@@ -0,0 +1,26 @@
+package com.example.demo.services;
+
+import com.example.demo.model.FamilyDB;
+import com.example.demo.model.MembersDB;
+import com.example.demo.model.MembersDTO;
+import com.example.demo.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+    MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void save(MembersDB membersDB) {
+        memberRepository.save(membersDB);
+    }
+
+    public List<MembersDB> getMemberDBByName(String name) {
+        return (List<MembersDB>) memberRepository.findByName(name);
+    }
+}
