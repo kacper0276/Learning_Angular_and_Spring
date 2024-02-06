76650c9869bed72a82f3d20923167def09dc252a
kacper0276 <kacper0276@op.pl>
2024-02-06 13:23:11 +0100

    Przekazywanie danych do formularza html - Następny odcinek: Iterowanie poprzez elementy

--- thymeleaf/src/main/java/com/example/thymeleaf/StudentService.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/StudentService.java
@@ -8,34 +8,14 @@ import java.util.UUID;
 
 @Service
 public class StudentService {
-
     private Set<Student> studentSet = new HashSet<>();
 
-    public StudentService(){
+    public Set<Student> getStudentSet() {
         Student student = new Student();
         student.setName("Adam");
         student.setSurname("Mickiewicz");
         student.setGrade("A");
-        student.setUuid(UUID.randomUUID().toString());
-
-        Student student1 = new Student();
-        student1.setName("Marek");
-        student1.setSurname("Wójtowicz");
-        student1.setGrade("C");
-        student1.setUuid(UUID.randomUUID().toString());
         studentSet.add(student);
-        studentSet.add(student1);
-    }
-
-    public Set<Student> getStudentSet(){
         return studentSet;
-    }
-    public void addStudent(Student student){
-        student.setUuid(UUID.randomUUID().toString());
-        studentSet.add(student);
-    }
-
-    public void remove(String uuid) {
-        studentSet.removeIf(value->value.getUuid().equals(uuid));
     }
 }
