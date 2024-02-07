34146823d44af9aac44397f9fe9e41ca24344f61
kacper0276 <kacper0276@op.pl>
2024-02-07 17:29:26 +0100

    Dalsza konfiguracja (angular material, bootstrap, ngrx), header - Następny odcinek: Utworzenie modułu Auth

--- projekt/frontend/src/app/app.module.ts
+++ projekt/frontend/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { StoreModule } from '@ngrx/store';
+import { CoreModule } from './modules/core/core.module';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+  ],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
