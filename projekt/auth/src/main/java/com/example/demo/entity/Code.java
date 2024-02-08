91c6994b097863e4caabd89744afafe8c95bf1c0
kacper0276 <kacper0276@op.pl>
2024-02-08 01:59:36 +0100

    Utworzenie modelu HttpResponse - Następny odcinek: Konfiguracja połączenia z bazą danych oraz utworzenie tabeli User

---
+++ projekt/auth/src/main/java/com/example/demo/entity/Code.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+public enum Code {
+    SUCCESS("Operation end success");
+
+    public final String label;
+    private Code(String label) {
+        this.label = label;
+    }
+}
