3f6a8dbed4942cee534d0175f10a4ebeabfa67d3
kacper0276 <kacper0276@op.pl>
2024-02-08 02:28:59 +0100

    Utworzenie serwisu JWT - Następny odcinek: Przeniesienie zmiennych do pliku konfiguracyjnego

---
+++ projekt/auth/src/main/java/com/example/demo/services/JwtService.java
@@ -0,0 +1,41 @@
+package com.example.demo.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtService {
+
+    public static final String SECRET = "AB8E50CEE980BC7BD72FB9A49B5262AD01FDF92498FF62C1B9E7E9193CCCCE8E";
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    public String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis())) // Kiedy utworzony
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+}
