7692ac06e24cafe66bf4cdc9073091b13e2dac26
kacper0276 <kacper0276@op.pl>
2024-02-08 12:17:06 +0100

    Usunięcie niepotrzebnych parametrów - Następny odcinek: Walidowanie tokenu

--- projekt/auth/src/main/java/com/example/demo/entity/UserRegisterDTO.java
+++ projekt/auth/src/main/java/com/example/demo/entity/UserRegisterDTO.java
@@ -1,15 +1,22 @@
 package com.example.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Email;
 
 @Getter
 @Setter
 @Builder
 public class UserRegisterDTO {
+    @Length(min = 5, max = 50, message = "Login powinien mieć od 5 do 50 znaków")
     private String login;
+    @Email
     private String email;
+    @JsonInclude(JsonInclude.Include.NON_NULL) // Jak wartość null to nie jest uwzględniona w requescie
+    @Length(min = 8, max = 75, message = "Hasło powinno składać się od 8 do 75 znaków")
     private String password;
     private Role role;
 }
