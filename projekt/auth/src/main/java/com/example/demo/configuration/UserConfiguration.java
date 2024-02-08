85daf36d8b19a28c248b12e7f66bdb6e30eb4bcc
kacper0276 <kacper0276@op.pl>
2024-02-08 11:20:08 +0100

    Dodanie endpointu do PermitAll listy - Następny odcinek: Konfiguracja Auth Provider

--- projekt/auth/src/main/java/com/example/demo/configuration/UserConfiguration.java
+++ projekt/auth/src/main/java/com/example/demo/configuration/UserConfiguration.java
@@ -2,11 +2,26 @@ package com.example.demo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class UserConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/v1/auth/register").permitAll()
+                );
+
+
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
