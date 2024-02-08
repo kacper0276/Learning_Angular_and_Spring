8526f142e544d49a83b3f40059bb9e621c9bf046
kacper0276 <kacper0276@op.pl>
2024-02-08 02:18:37 +0100

    Hashowanie hasła - Następny odcinek: Utworzenie serwisu JWT

--- projekt/auth/src/main/java/com/example/demo/services/UserService.java
+++ projekt/auth/src/main/java/com/example/demo/services/UserService.java
@@ -3,14 +3,17 @@ package com.example.demo.services;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.saveAndFlush(user); // Zapisuje i zwraca wynik z bazy danych
     }
 }
