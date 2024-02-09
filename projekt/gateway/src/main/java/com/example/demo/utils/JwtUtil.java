623920410fef81e0e3202a2e00de8e18dcb567f7
kacper0276 <kacper0276@op.pl>
2024-02-09 20:17:47 +0100

    Utworzenie metod do obsługi JWT - Następny odcinek: Utworzenie RouteValidatora

---
+++ projekt/gateway/src/main/java/com/example/demo/utils/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.example.demo.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import java.security.Key;
+
+public class JwtUtil {
+
+    public JwtUtil(@Value("${jwt.secret}") String secret){
+        SECRET = secret;
+    }
+
+    public final String SECRET ;
+
+    public void validateToken(final String token){
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey(){
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
