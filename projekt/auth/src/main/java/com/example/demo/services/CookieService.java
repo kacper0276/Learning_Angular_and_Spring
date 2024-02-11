2d5ccd67bb99cb672848a6cdfe328008be0d18f3
kacper0276 <kacper0276@op.pl>
2024-02-12 00:55:46 +0100

    Wylogowywanie - Następny odcinek: [FE] Moduł auth, shared, material oraz komponent logowania i rejestracji

--- projekt/auth/src/main/java/com/example/demo/services/CookieService.java
+++ projekt/auth/src/main/java/com/example/demo/services/CookieService.java
@@ -13,4 +13,15 @@ public class CookieService {
         return cookie;
     }
 
+    public Cookie removeCookie(Cookie[] cookies, String name){
+        for (Cookie cookie:cookies){
+            if (cookie.getName().equals(name)){
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+                cookie.setHttpOnly(true);
+                return cookie;
+            }
+        }
+        return null;
+    }
 }
