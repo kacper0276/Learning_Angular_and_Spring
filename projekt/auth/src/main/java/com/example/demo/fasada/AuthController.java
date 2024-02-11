9076a55330a67efbfbc5ba718a977d04e02967a4
kacper0276 <kacper0276@op.pl>
2024-02-12 00:54:47 +0100

    Konfiguracja CORS - Następny odcinek: Wylogowywanie

--- projekt/auth/src/main/java/com/example/demo/fasada/AuthController.java
+++ projekt/auth/src/main/java/com/example/demo/fasada/AuthController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+//@CrossOrigin // Wszystko na *
+//@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     private final UserService userService;
