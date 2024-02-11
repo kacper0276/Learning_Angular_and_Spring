a0fbada99651a3c863fa5caaa17710f7c3085545
kacper0276 <kacper0276@op.pl>
2024-02-11 23:56:46 +0100

    Zmiana w logice logowania dla nieaktywnych kont - Następny odcinek: Wysyłanie maila na żądanie zmiany hasła

--- projekt/auth/src/main/java/com/example/demo/repository/UserRepository.java
+++ projekt/auth/src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
      Optional<User> findUserByLogin(String login);
      Optional<User> findUserByEmail(String email);
      Optional<User> findUserByUuid(String uuid);
+     @Query(nativeQuery = true, value = "SELECT * FROM users where login=?1 and islock=false and isenabled=true")
      Optional<User> findUserByLoginAndLockAndEnabled(String login);
 }
