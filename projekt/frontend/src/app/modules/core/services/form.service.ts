ad3541ba3221b097147241d106167948be435f5e
kacper0276 <kacper0276@op.pl>
2024-02-13 01:52:43 +0100

    Formularz rejestracji oraz logowania w kodzie TS oraz walidatory - Następny odcinek: Formularze do resetowania hasła oraz własny walidator

--- projekt/frontend/src/app/modules/core/services/form.service.ts
+++ projekt/frontend/src/app/modules/core/services/form.service.ts
@@ -1,8 +1,83 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginForm } from '../models/forms.model';
+import { RegisterForm } from '../models/forms.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FormService {
-  constructor() {}
+  initLoginForm(): FormGroup<LoginForm> {
+    return new FormGroup({
+      login: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(50),
+        ],
+        nonNullable: true,
+      }),
+      password: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(75),
+        ],
+        nonNullable: true,
+      }),
+    });
+  }
+
+  initRegisterForm(): FormGroup<RegisterForm> {
+    return new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+        nonNullable: true,
+      }),
+      login: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(50),
+        ],
+        nonNullable: true,
+      }),
+      password: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(75),
+        ],
+        nonNullable: true,
+      }),
+      repeatedPassword: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(75),
+        ],
+        nonNullable: true,
+      }),
+    });
+  }
+
+  getErrorMessage(control: FormControl): string {
+    if (control.hasError('required')) {
+      return 'Ta kontrolka jest wymagana';
+    }
+
+    if (control.hasError('minlength')) {
+      return `Minimalna ilość znaków: ${control.errors?.['minlength']?.requiredLength}.`;
+    }
+
+    if (control.hasError('maxlength')) {
+      return `Maksymalna ilość znaków: ${control.errors?.['maxlength']?.requiredLength}.`;
+    }
+
+    if (control.hasError('email')) {
+      return `Niepoprawny adres e-mail.`;
+    }
+
+    return '';
+  }
 }
