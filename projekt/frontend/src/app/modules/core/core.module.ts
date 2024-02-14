24f7d9ed82d04136e5ce42069362f533ec31a40d
kacper0276 <kacper0276@op.pl>
2024-02-14 12:57:03 +0100

    Dodanie login$ i register$ do AuthEffects. Konfiguracja notifier'a - Następny odcinek: Dokonczenie rejestracji (pierwszy użytkownik) i error handling interceptor

--- projekt/frontend/src/app/modules/core/core.module.ts
+++ projekt/frontend/src/app/modules/core/core.module.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NotifierModule, NotifierOptions } from 'angular-notifier';
 
 @NgModule({
   declarations: [HeaderComponent],
