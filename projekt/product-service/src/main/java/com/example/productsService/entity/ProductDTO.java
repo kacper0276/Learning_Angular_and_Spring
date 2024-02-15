9be1178efc0894b23969a5178a870d25c9b9212d
kacper0276 <kacper0276@op.pl>
2024-02-15 14:32:19 +0100

    Utworzenie modeli danych - Następny odcinek: Utworzenie tabel w bazie danych

---
+++ projekt/product-service/src/main/java/com/example/productsService/entity/ProductDTO.java
@@ -0,0 +1,5 @@
+package com.example.productsService.entity;
+
+public class ProductDTO extends Product{
+    private long categoryID;
+}
---
+++ projekt/product-service/src/main/java/com/example/productsService/entity/ProductEntity.java
@@ -0,0 +1,5 @@
+package com.example.productsService.entity;
+
+public class ProductEntity extends Product{
+    private Category category;
+}
