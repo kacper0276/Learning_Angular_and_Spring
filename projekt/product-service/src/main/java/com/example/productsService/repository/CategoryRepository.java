ef8680ed7c976168a6f83535a4f0db5ed547fe1c
kacper0276 <kacper0276@op.pl>
2024-02-15 15:08:35 +0100

    Utworzenie repozytorium dla utworzonych tabel - Następny odcinek: Utworzenie Fasady

---
+++ projekt/product-service/src/main/java/com/example/productsService/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.productsService.repository;
+
+import com.example.productsService.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ projekt/product-service/src/main/java/com/example/productsService/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.productsService.repository;
+
+import com.example.productsService.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+}
