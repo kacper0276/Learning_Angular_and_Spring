9365cb5f49a02b1ff02eb7da911c0f6127f61f4d
kacper0276 <kacper0276@op.pl>
2024-02-16 00:35:25 +0100

    Utworzenie mechanizmu wyszukiwarki produktów - Następny odcinek: Implementacja mechanizmu wyszukiwania po kategorii

--- projekt/product-service/src/main/java/com/example/productsService/mediator/ProductMediator.java
+++ projekt/product-service/src/main/java/com/example/productsService/mediator/ProductMediator.java
@@ -1,17 +1,21 @@
 package com.example.productsService.mediator;
 
+import com.example.productsService.entity.ProductEntity;
 import com.example.productsService.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class ProductMediator {
     private final ProductService productService;
 
-    public ResponseEntity<?> getProduct(int age, int limit) {
+    public ResponseEntity<?> getProduct(int age, int limit, String name, String category, Float price_min, Float price_max, String data) {
         long totalCount = productService.countActiveProducts();
-        return ResponseEntity.ok().header("X-Total-Count", String.valueOf(totalCount)).body("");
+        List<ProductEntity> product = productService.getProduct(name,category,price_min,price_max,data);
+        return ResponseEntity.ok().header("X-Total-Count", String.valueOf(totalCount)).body(product);
+
     }
 }
--- projekt/product-service/src/main/java/com/example/productsService/service/ProductService.java
+++ projekt/product-service/src/main/java/com/example/productsService/service/ProductService.java
@@ -1,20 +1,57 @@
 package com.example.productsService.service;
 
-import com.example.productsService.entity.ProductDTO;
+import com.example.productsService.entity.ProductEntity;
 import com.example.productsService.repository.ProductRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+    @PersistenceContext
+    EntityManager entityManager;
     private final ProductRepository productRepository;
 
     public long countActiveProducts() {
         return productRepository.countActiveProducts();
     }
 
-    public ProductDTO getProductDto() {
-        return null;
+    public List<ProductEntity> getProduct(String name, String category, Float price_min, Float price_max, String data){
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<ProductEntity> query = criteriaBuilder.createQuery(ProductEntity.class);
+        Root<ProductEntity> root = query.from(ProductEntity.class);
+        List<Predicate> predicates = new ArrayList<>();
+        if (data != null && !data.equals("") && name != null && !name.trim().equals("")){
+            DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+            LocalDate date = LocalDate.parse(data, inputFormatter);
+            return productRepository.findByNameAndCreateAt(name,date);
+        }
+        if (name != null && !name.trim().equals("")) {
+            predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + name.toLowerCase() + "%"));
+        }
+        if (category != null && !category.equals("")) {
+            predicates.add(criteriaBuilder.equal(root.get("category"), category));
+        }
+        if (price_min != null) {
+            predicates.add(criteriaBuilder.greaterThan(root.get("price"), price_min-0.01));
+        }
+        if (price_max != null) {
+            predicates.add(criteriaBuilder.lessThan(root.get("price"), price_max+0.01));
+        }
+        query.where(predicates.toArray(new Predicate[0]));
+
+        return entityManager.createQuery(query).getResultList();
     }
+
 }
