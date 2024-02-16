e660e3442d539bd87727a03e2fb6b8fefc0ae449
kacper0276 <kacper0276@op.pl>
2024-02-16 01:24:17 +0100

    Dekodowanie wartości z URL - Następny odcinek: Utworzenie modelu Produktu "Simple"

--- projekt/product-service/src/main/java/com/example/productsService/mediator/ProductMediator.java
+++ projekt/product-service/src/main/java/com/example/productsService/mediator/ProductMediator.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 @Component
 @RequiredArgsConstructor
@@ -15,6 +17,14 @@ public class ProductMediator {
     public ResponseEntity<?> getProduct(int page, int limit, String name, String category, Float price_min, Float price_max, String data, String sort, String order) {
         long totalCount = productService.countActiveProducts(name, category, price_min, price_max);
         List<ProductEntity> product = productService.getProduct(name,category,price_min,price_max,data, page, limit, sort, order);
+        if (name != null && !name.isEmpty()){
+            try {
+                name = URLDecoder.decode(name, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
         return ResponseEntity.ok().header("X-Total-Count", String.valueOf(totalCount)).body(product);
 
     }
