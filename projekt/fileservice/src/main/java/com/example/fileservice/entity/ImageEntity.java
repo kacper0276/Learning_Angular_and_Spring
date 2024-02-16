2ae95d5efd905e35553e08824c77eaa3add2e389
kacper0276 <kacper0276@op.pl>
2024-02-16 02:32:15 +0100

    Utworzenie modelu domenowego pliku - Następny odcinek: Utworzenie logiki przesyłania plików

---
+++ projekt/fileservice/src/main/java/com/example/fileservice/entity/ImageEntity.java
@@ -0,0 +1,11 @@
+package com.example.fileservice.entity;
+
+import java.time.LocalDate;
+
+public class ImageEntity {
+    private long id;
+    private String uuid;
+    private String path;
+    private boolean isUsed;
+    private LocalDate createAt;
+}
