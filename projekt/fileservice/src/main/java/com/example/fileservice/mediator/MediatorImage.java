a65e6781d9781d27298ef80c4453fa9671aad65a
kacper0276 <kacper0276@op.pl>
2024-02-17 02:06:06 +0100

    Utworzenie MediatorImage - Następny odcinek: Utworzenie i wykorzystanie ImageDTO

---
+++ projekt/fileservice/src/main/java/com/example/fileservice/mediator/MediatorImage.java
@@ -0,0 +1,26 @@
+package com.example.fileservice.mediator;
+
+import com.example.fileservice.entity.ImageEntity;
+import com.example.fileservice.service.FtpService;
+import com.example.fileservice.service.ImageService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+@AllArgsConstructor
+public class MediatorImage {
+    private final FtpService ftpService;
+    private final ImageService imageService;
+
+    public ResponseEntity<?> saveImage(MultipartFile file) {
+        try {
+            ImageEntity imageEntity = ftpService.uploadFileToFtp(file);
+            imageService.save(imageEntity);
+            return ResponseEntity.ok("");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(400).body("Nie udało sie");
+        }
+    }
+}
---
+++ projekt/fileservice/src/main/java/com/example/fileservice/service/ImageService.java
@@ -0,0 +1,16 @@
+package com.example.fileservice.service;
+
+import com.example.fileservice.entity.ImageEntity;
+import com.example.fileservice.repository.ImageRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ImageService {
+    private final ImageRepository imageRepository;
+
+    public ImageEntity save(ImageEntity imageEntity) {
+        return imageRepository.saveAndFlush(imageEntity);
+    }
+}
