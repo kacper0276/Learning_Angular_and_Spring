89018e162f893ad6ba20884448646131bb2dd94f
kacper0276 <kacper0276@op.pl>
2024-02-18 08:55:13 +0100

    Usuwanie nieużywanych plików - Następny odcinek: Dodawanie produktów

--- projekt/fileservice/src/main/java/com/example/fileservice/service/ImageService.java
+++ projekt/fileservice/src/main/java/com/example/fileservice/service/ImageService.java
@@ -3,12 +3,18 @@ package com.example.fileservice.service;
 import com.example.fileservice.entity.ImageEntity;
 import com.example.fileservice.repository.ImageRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
+@EnableScheduling
 @AllArgsConstructor
 public class ImageService {
     private final ImageRepository imageRepository;
+    private final FtpService ftpService;
 
     public ImageEntity save(ImageEntity imageEntity) {
         return imageRepository.saveAndFlush(imageEntity);
@@ -16,5 +22,17 @@ public class ImageService {
 
     public ImageEntity findByUuid(String uuid) {
         return imageRepository.findByUuid(uuid).orElse(null);
+    }
+
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void cleanImage() {
+        imageRepository.findDontUseImages().forEach(value -> {
+            try {
+                ftpService.deleteFile(value.getPath());
+                imageRepository.delete(value);
+            } catch (IOException e) {
+                System.out.println("Can't delete " + value.getUuid());
+            }
+        });
     }
 }
