c6195b2340801df0b710e0c8c931d39effd96e98
kacper0276 <kacper0276@op.pl>
2024-02-24 12:04:09 +0100

    Upload zdjęć oraz ich usuwanie w formularzu dodawania produktów - Następny odcinek: Upload zdjęć oraz ich usuwanie w formularzu dodawania produktów

--- projekt/frontend/src/material.module.ts
+++ projekt/frontend/src/material.module.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   exports: [
@@ -14,6 +15,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatPaginatorModule,
     MatAutocompleteModule,
     MatSelectModule,
+    MatExpansionModule,
   ],
 })
 export class MaterialModule {}
