337eabaa10b87ace34919a26f563c829004c47a3
kacper0276 <kacper0276@op.pl>
2024-02-26 09:00:43 +0100

    Dodawanie produktów do koszyka - Następny odcinek: Utworzenie beana Rest Template

--- projekt/basket/src/main/java/com/example/basket/repository/BasketRepository.java
+++ projekt/basket/src/main/java/com/example/basket/repository/BasketRepository.java
@@ -3,5 +3,9 @@ package com.example.basket.repository;
 import com.example.basket.entity.Basket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BasketRepository extends JpaRepository<Basket, Long> {
+import java.util.Optional;
+
+public interface BasketRepository extends JpaRepository<Basket,Long> {
+    Optional<Basket> findByUuid(String uuid);
 }
+
