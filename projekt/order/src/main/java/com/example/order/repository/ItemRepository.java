82c965c35085cae20dea509d1f6a83b569e19adf
kacper0276 <kacper0276@op.pl>
2024-03-11 20:40:38 +0100

    Notyfikacje PayU - Następny odcinek: Pobieranie informacji o zamówieniach

--- projekt/order/src/main/java/com/example/order/repository/ItemRepository.java
+++ projekt/order/src/main/java/com/example/order/repository/ItemRepository.java
@@ -1,7 +1,11 @@
 package com.example.order.repository;
 
+import com.example.order.entity.Order;
 import com.example.order.entity.OrderItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<OrderItems, Long> {
+    Optional<OrderItems> findOrderItemsByOrder(Order order);
 }
